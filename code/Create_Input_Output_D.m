35b7ee0db7689b43a6303a0bfb409a8fcf401ec9
rfernand387 <55104357+rfernand387@users.noreply.github.com>
2020-02-26 13:26:56 -0500

    Add files via upload

--- code/Create_Input_Output_D.m
+++ code/Create_Input_Output_D.m
@@ -81,7 +81,7 @@ if (Debug)
     disp(['Simulating ' num2str(Nb_Sims) ' cases for Class ' num2str(Class)])
 end
 lambdaref = 799;
-for isim=1:Nb_Sims % boucle sur les simulations
+parfor isim=1:Nb_Sims % boucle sur les simulations
     if strcmp(Def_Base.RTM,'sail3')
         
         %  propriétés des feuilles et du sol
@@ -136,18 +136,9 @@ for isim=1:Nb_Sims % boucle sur les simulations
         system(['xcopy ',templatedir,' ',targetdir,'  /e /q']);
         
         %  Reflectance top of canopy done in three stages due to FLIGHT
-        [ R((400:600)-399,:) D(isim,1) ] = doflightr1d(targetdir,400:600,lambdaref,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim), ...
-                                            Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim), ...
-                                            Law.rl(isim),Law.gamma(isim),Law.fbark(isim),Law.fsen(isim),Law.rxy(isim),Law.rz(isim),Law.Min_ht(isim),Law.Max_ht(isim), ...
-                                            Law.Sun_Zenith(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)])
-        [ R((1001:1600)-399,:) x ] = doflightr1d(targetdir,1001:1600,1600,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),...
-                                            Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim), ...
-                                            Law.rl(isim),Law.gamma(isim),Law.fbark(isim),Law.fsen(isim),Law.rxy(isim),Law.rz(isim),Law.Min_ht(isim),Law.Max_ht(isim), ...
-                                            Law.Sun_Zenith(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)]);
-        [ R((1601:2200)-399,:) x ] = doflightr1d(targetdir,1601:2200,2200,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),...
-                                            Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim), ...
-                                            Law.rl(isim),Law.gamma(isim),Law.fbark(isim),Law.fsen(isim),Law.rxy(isim),Law.rz(isim),Law.Min_ht(isim),Law.Max_ht(isim), ...                                            
-                                            Law.Sun_Zenith(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)]);
+        [ R((400:600)-399,:) D(isim,1) ] = doflightr1d(targetdir,400:600,lambdaref,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim),Law.Sun_Zenith(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)])
+        [ R((1001:1600)-399,:) x ] = doflightr1d(targetdir,1001:1600,1600,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim),Law.Sun_Zenith(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)]);
+        [ R((1601:2200)-399,:) x ] = doflightr1d(targetdir,1601:2200,2200,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim),Law.Sun_Zenith(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)]);
         
         % For fAPAR
         
@@ -167,10 +158,7 @@ for isim=1:Nb_Sims % boucle sur les simulations
         FCOVER(isim,1) =Law.Crown_Cover(isim).*(1-exp(-kellips(Law.ALA(isim),0).*Law.LAI(isim)));
         
         %  fAPAR (black-sky à 10h)
-        [ A((400:700)-399,:) x ] = doflightr1d(targetdir,400:700,700,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),...
-                                                Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim),...
-                                                Law.rl(isim),Law.gamma(isim),Law.fbark(isim),Law.fsen(isim),Law.rxy(isim),Law.rz(isim),Law.Min_ht(isim),Law.Max_ht(isim), ...
-                                                Law.Sun_Zenith_FAPAR(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)]);
+        [ A((400:700)-399,:) x ] = doflightr1d(targetdir,400:700,700,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim),Law.Sun_Zenith_FAPAR(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)]);
         Ecl=Sol_Irr(:,round(Law.Sun_Zenith(isim)./5)+1);
         FAPAR(isim,1)  = sum(A(1:300,5).*Ecl(1:300))./sum(Ecl(1:300)); % valeur intégrée entre 400 et 700 nm
         
@@ -182,7 +170,7 @@ for isim=1:Nb_Sims % boucle sur les simulations
         delete([targetdir,'\DATA\*.*']);
         delete([targetdir,'\SPEC\*.*']);
         
-    elseif strcmp(Def_Base.RTM,'FLIGHT')
+    elseif strcmp(Def_Base.RTM,'flight')
         
         %  propriétés des feuilles et du sol
         Car = Law.Cab(isim)/4;
@@ -193,29 +181,17 @@ for isim=1:Nb_Sims % boucle sur les simulations
         
         % point to flight directories and makre a temporary targetdir
         templatedir = '.\code\FLIGHTFORWARD3D';
-        targetdirmaster = '.\code\FLIGHTTARGET10';
+        targetdirmaster = '.\code\FLIGHTTARGET30';
         targetdir = [targetdirmaster,num2str(isim)];
         mkdir([targetdir]);
         system(['xcopy ',templatedir,' ',targetdir,'  /e /q']);
         
         %  Reflectance top of canopy done in  stages due to FLIGHT
-        [ R((400:999)-399,:) D(isim,1) ] = doflight(targetdir,400:999,lambdaref,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),...
-                                                    Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim),...
-                                                    Law.rleaf(isim),Law.gamma(isim),Law.fbark(isim),Law.fsen(isim),Law.rxy(isim),Law.rz(isim),Law.Min_ht(isim),Law.Max_ht(isim), ...
-                                                    Law.Sun_Zenith(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)])
+        [ R((400:999)-399,:) D(isim,1) ] = doflight(targetdir,400:999,lambdaref,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim),Law.Sun_Zenith(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)])
         A = R((400:699)-399,5);
-        [ R((1000:1599)-399,:) x ] = doflight(targetdir,1000:1599,lambdaref,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),...
-                                                    Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim),...
-                                                    Law.rleaf(isim),Law.gamma(isim),Law.fbark(isim),Law.fsen(isim),Law.rxy(isim),Law.rz(isim),Law.Min_ht(isim),Law.Max_ht(isim), ...
-                                                    Law.Sun_Zenith(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)]);
-        [ R((1600:2199)-399,:) x ] = doflight(targetdir,1600:2199,lambdaref,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),...
-                                                    Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim), ...
-                                                    Law.rleaf(isim),Law.gamma(isim),Law.fbark(isim),Law.fsen(isim),Law.rxy(isim),Law.rz(isim),Law.Min_ht(isim),Law.Max_ht(isim), ...
-                                                    Law.Sun_Zenith(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)]);
-        [ R((1801:2400)-399,:) x ] = doflight(targetdir,1801:2400,lambdaref,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),...
-                                                    Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim),...
-                                                    Law.rleaf(isim),Law.gamma(isim),Law.fbark(isim),Law.fsen(isim),Law.rxy(isim),Law.rz(isim),Law.Min_ht(isim),Law.Max_ht(isim), ...
-                                                    Law.Sun_Zenith(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)]);
+        [ R((1000:1599)-399,:) x ] = doflight(targetdir,1000:1599,lambdaref,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim),Law.Sun_Zenith(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)]);
+        [ R((1600:2199)-399,:) x ] = doflight(targetdir,1600:2199,lambdaref,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim),Law.Sun_Zenith(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)]);
+        [ R((1801:2400)-399,:) x ] = doflight(targetdir,1801:2400,lambdaref,Law.N(isim),Law.Cab(isim),Car,Ant,Law.Cbp(isim),Law.Cw(isim),Law.Cdm(isim),Law.ALA(isim),LIDFb,Law.LAI(isim),Law.HsD(isim),Law.Crown_Cover(isim),Law.Sun_Zenith(isim),Law.View_Zenith(isim),Law.Rel_Azimuth(isim),[Def_Base.(['Class_' num2str(Class)]).R_Soil.Lambda(51:2051) Rs(:,1)]);
        
         % For fAPAR
         
